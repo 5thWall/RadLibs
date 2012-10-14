5ab6fc87a67a8252591d2f3b1ca42b86cf2d0afe
Andy <5thWall@gmail.com>
2012-10-13 22:56:44 -0700

    Don't allow templates to be edited after publish

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -2,28 +2,19 @@ class TemplatesController < ApplicationController
 
   def index
     @templates = Template.all
-
   end
 
   def show
-    @template = Template.find(params[:id]) 
-
-  end
-
-  def new
-  if user_signed_in?
-    @template = Template.new
-  else
-  redirect_to templates_path
-  end
-
-  end
-
-
-  def edit
     @template = Template.find(params[:id])
   end
 
+  def new
+    if user_signed_in?
+      @template = Template.new
+    else
+      redirect_to templates_path
+    end
+  end
 
   def create
   	@user = current_user
@@ -37,33 +28,17 @@ class TemplatesController < ApplicationController
       end
    end
 
-
-  def update
-    @template = Template.find(params[:id])
-
-   
-      if @template.update_attributes(params[:template])
-        redirect_to @template, notice: 'Template was successfully updated.' 
-       
-      else
-        render "edit" 
-     
-      end
-    end
-
-
   def destroy
     @template = Template.find(params[:id])
     @template.destroy
+  	redirect_to templates_url
+  end
 
-	redirect_to templates_url
-    end
 
-  
   def vote
   	value = params[:type] == "up" ? 1 : -1
   	@template = Template.find(params[:id])
     @template.add_evaluation(:votes, value, current_user)
-      redirect_to :back, notice: "Thank you for voting."
+    redirect_to :back, notice: "Thank you for voting."
   end
 end
