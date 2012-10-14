0e41b4525ed2f2c510a7c5daf0d1027450ed59e9
Louis Rudner <louisrudner@gmail.com>
2012-10-14 17:01:38 +1100

    started on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
+    @toprated = User.find_with_reputation(:votes, :all, {:conditions => ["votes > ?", 0], :order => "votes desc", :limit => 5})
+    @toptemplates = Template.find_with_reputation(:votes, :all, {:conditions => ["votes > ?", 0], :order => "votes desc", :limit => 5})
   end
 end
