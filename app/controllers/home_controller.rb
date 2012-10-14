313622359f39ad9c508e11ce1ef3c5e69387711b
Louis Rudner <louisrudner@gmail.com>
2012-10-14 18:24:13 +1100

    more style changes, fixed navbar doesnt get in the way now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @toprated = User.find_with_reputation(:votes, :all, {:conditions => ["votes > ?", 0], :order => "votes desc", :limit => 5})
     @toptemplates = Template.find_with_reputation(:votes, :all, {:conditions => ["votes > ?", 0], :order => "votes desc", :limit => 5})
+     @recenttemplates = Template.where(:created_at => (Time.now - 24.hours)..(Time.now)).limit(5)
+     @recentradlibs = Radlib.where(:created_at => (Time.now - 24.hours)..(Time.now)).limit(5)
   end
 end
