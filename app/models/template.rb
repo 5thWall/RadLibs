5450427094424c27a831bd12a638a629eb07f5d7
Andy <5thWall@gmail.com>
2012-10-18 20:48:57 -0700

    Cleaned up code

--- app/models/template.rb
+++ app/models/template.rb
@@ -2,7 +2,7 @@ class Template < ActiveRecord::Base
   attr_accessible  :title, :template, :user_id
   has_many :radlibs
   belongs_to :user
- 
+
   has_reputation :votes, source: :user, aggregated_by: :sum
   
 end
