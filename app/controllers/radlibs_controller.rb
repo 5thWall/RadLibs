d788ed45f95cb030c6ec406796f90e9143411766
Andy <5thWall@gmail.com>
2012-10-20 15:27:05 -0700

    Radlibs controler loaded and authorized with cancan

--- app/controllers/radlibs_controller.rb
+++ app/controllers/radlibs_controller.rb
@@ -1,50 +1,29 @@
 class RadlibsController < ApplicationController
-  # GET /radlibs
-  # GET /radlibs.json
+  load_and_authorize_resource
+
   def index
     @radlibs = Radlib.find(:all, :order => "created_at desc")
   end
 
-  # GET /radlibs/1
-  # GET /radlibs/1.json
   def show
     @radlib = Radlib.find(params[:id])
   end
 
-  # GET /radlibs/new
-  # GET /radlibs/new.json
   def new
-    redirect_to root_url, alert: 'Please log in before doing that' unless current_user
-
-    @template = Template.find params[:template_id]
-    @radlib = @template.radlibs.build
+    @radlib.template = Template.find params[:template_id]
   end
 
-  # GET /radlibs/1/edit
-  def edit
-    redirect_to radlibs_path
-  end
-
-  # POST /radlibs
-  # POST /radlibs.json
   def create
-    redirect_to root_url, alert: "Please log in before doing that" unless current_user
-
-    @radlib = Radlib.new(params[:radlib])
+    @radlib.user = current_user
 
     if @radlib.save
-    current_user.radlibs << @radlib
       redirect_to template_path(@radlib.template), notice: 'Radlib was successfully created.'
     else
       render action: "new"
     end
   end
 
-  # PUT /radlibs/1
-  # PUT /radlibs/1.json
   def update
-    @radlib = Radlib.find(params[:id])
-
     if @radlib.update_attributes(params[:radlib])
       redirect_to @radlib, notice: 'Radlib was successfully updated.'
     else
@@ -52,18 +31,13 @@ class RadlibsController < ApplicationController
     end
   end
 
-  # DELETE /radlibs/1
-  # DELETE /radlibs/1.json
   def destroy
-    @radlib = Radlib.find(params[:id])
     @radlib.destroy
-
     redirect_to radlibs_url
   end
 
   def vote
   	value = params[:type] == "up" ? 1 : -1
-  	@radlib = Radlib.find(params[:id])
     @radlib.add_or_update_evaluation(:votes, value, current_user)
     redirect_to :back, notice: "Thank you for voting."
   end
