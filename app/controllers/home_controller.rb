08f5f921d92d3f1fa671b4ddbe52324a63aaa0e4
Andy <5thWall@gmail.com>
2012-10-24 23:26:50 -0700

    Sope toprated radlibs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @toprated = User.toprated
     @toptemplates = Template.toprated
-    @topradlibs = Radlib.find_with_reputation(:votes, :all, {:conditions => ["votes > ?", 0], :order => "votes desc", :limit => 5})
+    @topradlibs = Radlib.toprated
     @recenttemplates = Template.find(:all, :order => "id desc", :limit => 5).reverse
     @recentradlibs = Radlib.find(:all, :order => "id desc", :limit => 5).reverse
   end
--- app/models/radlib.rb
+++ app/models/radlib.rb
@@ -2,6 +2,11 @@ class Radlib < ActiveRecord::Base
   attr_accessible :template_id, :user_id, :words
   belongs_to :template
   belongs_to :user
-  
-    has_reputation :votes, source: :user, aggregated_by: :sum
+
+  has_reputation :votes, source: :user, aggregated_by: :sum
+
+  scope :toprated, find_with_reputation(:votes, :all,
+    {:conditions => ["votes > ?", 0],
+      :order => "votes desc",
+      :limit => 5})
 end
